b5baff7cb62f79292e95fe8128742d13044d346b
Yogesh <myogeshchandra8@gmail.com>
2026-05-02 10:34:56 +0530

    Basic oops

---
+++ src/Basics/Test.java
@@ -0,0 +1,28 @@
+package Basics;
+
+class A{
+
+}
+class B{
+
+}
+class C{
+
+}
+// file name is Test.java but no test class
+// because there is no any public class
+//if any public class present file name also be same as that public class name
+// here i declared class d as public you can see error.
+//any class have atmost one public class (either 0 or 1)
+/*
+
+
+public class D{
+    Test.java:15: error: class D is public, should be declared in a file named D.java
+    public class D{
+       ^
+               1 error
+
+}
+
+*/
